15f6adfcfdac79606366346e4b755e6fcfd281dd
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:42:56 +0000

    Whitelist columns in Destination for ActiveAdmin

--- app/admin/destinations.rb
+++ app/admin/destinations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Destination do
+
+ permit_params :name, :description, :tag_id, :image_url, :latitude, :longitude
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
