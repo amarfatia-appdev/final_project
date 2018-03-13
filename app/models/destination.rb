3aa1e833253fba2bfd82eed20f43f56b51bb9fd8
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:42:40 +0000

    Direct association between destinations and tags

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,9 @@
 class Destination < ApplicationRecord
   # Direct associations
 
+  has_many   :tags,
+             :dependent => :destroy
+
   has_many   :flights,
              :dependent => :destroy
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ApplicationRecord
   # Direct associations
 
+  belongs_to :destination
+
   # Indirect associations
 
   # Validations
