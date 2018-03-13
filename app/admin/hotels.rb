adf2be425cc4c1f5f230bb3dc3caaf87bad8c4db
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:43:02 +0000

    Whitelist columns in Hotel for ActiveAdmin

--- app/admin/hotels.rb
+++ app/admin/hotels.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Hotel do
+
+ permit_params :stars, :min_cost, :destination_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
